2b130b4760e157376ce79955a152b63164b933ed
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-07-04 15:50:41 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MyApp());
 int _selectedIndex = 0; //New
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,11 +18,27 @@ class MyApp extends StatelessWidget {
 }
 
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+//bottomNavigationBar
+class _HomePageState extends State<HomePage> {
+  var _formKey = GlobalKey<FormState>();
+
+  int _indiceAtual = 1;
+  final List<Widget> _telas = [
+    //NewPageScreen("Meus pedidos"),
+    //NewPageScreen("teste 2"),
+    //NewPageScreen("Favoritos")
+  ];
+
 /*class NewPageScreen extends StatelessWidget { //classe para a mudança de página ao clicar o botao do BottomNavigationBar
   final String texto;
-
   NewPageScreen(this.texto);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,28 +50,13 @@ class MyApp extends StatelessWidget {
 }*/
 
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-
-class _HomePageState extends State<HomePage> {
-  var _formKey = GlobalKey<FormState>();
-  var isLoading = false;
-
-  int _indiceAtual = 1; //bottomNavigation / Contador
-  final List<Widget> _telas = [//bottomNavigation
-    //NewPageScreen("Meus pedidos"),
-    //NewPageScreen("teste 2"),
-    //NewPageScreen("Favoritos")
-  ]; //bottomNavigation
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF25305),
-      //onTap: onTabTapped,
+
+
+
       appBar: AppBar(
         leading: Icon(
             Icons.arrow_back_sharp, color: Color(0xFF571F04), size: 45),
@@ -61,9 +64,10 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: const Color(0xFFF4DE9C),
         toolbarHeight: 120,
 
-        title: Text("GALIF", style: TextStyle(fontSize: 32.0,
-          fontWeight: FontWeight.bold,
-          color: Color(0xFF571F04),),),
+        title: Text("GALIF", style:
+          TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Color(0xFF571F04),
+          ),
+        ),
 
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(
@@ -72,37 +76,45 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
 
+
+
       body: Padding(
+       //child: SingleChildScrollView(
+
         padding: const EdgeInsets.all(30.0),
-        //form
         child: Form(
           key: _formKey,
           child: Column(
             children: <Widget>[
-              Image.network(
-                'https://i.ibb.co/ZzCVW80/gabiogol.jpg',
-                width: 200,
-                height: 300,
-                //fit: BoxFit.fitHeight,
+              Text ("CALENDÁRIO", style: TextStyle
+                (fontSize: 26.0, fontWeight: FontWeight.bold, color: Color(0xFF571F04),),
               ),
 
-              SizedBox(height: 2),
+              SizedBox(height: 10),
 
+              Image.network('https://i.ibb.co/9qh2ZBg/Design-sem-nome.png', width: 335, height: 335,),
 
-              ElevatedButton(
-                onPressed: onPressedButton,
-                child: const Text(
-                  'INSERIR',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color(0xFF571F04),
+              SizedBox(height: 10),
+
+              Container(
+                width: 130,
+                child: ElevatedButton(
+                  onPressed: onPressedButton,
+                  child: Row(
+                    children: [
+                      Text('Junho', style: TextStyle (fontSize: 20, color: Color(0xFF571F04),),),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.arrow_drop_down_outlined, color: Color(0xFF571F04)),
+                      ),
+                    ],
                   ),
-                ),
 
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFF4DE9C),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFF4DE9C),
+                    shape: RoundedRectangleBorder(
+                    ),
                   ),
                 ),
               ),
@@ -110,6 +122,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+
 
 
       bottomNavigationBar: BottomNavigationBar(
@@ -138,12 +151,11 @@ class _HomePageState extends State<HomePage> {
 
 
   void onTabTapped(int index) { //colocar a ação que será feita ao clicar nos botões
-    setState(() {
-      _indiceAtual = index;
-    });
+    setState(()
+      {_indiceAtual = index;}
+    );
   }
 
 
   void onPressedButton() {}
-
 }
